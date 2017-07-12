066b023e568c325d3349697bc9e671e52ce5de54
n00bmind <n00bmindr0b0t@gmail.com>
2017-07-12 19:48:10 +0200

    no message

--- src/opengl_renderer.cpp
+++ src/opengl_renderer.cpp
@@ -46,6 +46,6 @@ internal void
 OpenGLRenderToOutput( GameRenderCommands *commands )
 {
     glViewport( 0, 0, commands->width, commands->height );
-    glClearColor( 1.0f, 0.0f, 1.0f, 0.0f );
+    glClearColor( 0.97f, 0.97f, 0.97, 1.0f );
     glClear( GL_COLOR_BUFFER_BIT );
 }
