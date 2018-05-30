f25da5f053f5876ecc036d34e942c1b9d3302955
n00bmind <n00bmindr0b0t@gmail.com>
2018-05-30 14:04:17 +0100

    * Specify CRT_SECURE_NO_WARNINGS for release too

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ set RRBIN=bin
 
 set LIBS=user32.lib gdi32.lib winmm.lib ole32.lib opengl32.lib shlwapi.lib
 set WARNINGFLAGS=-wd4201 -wd4100 -wd4189 -wd4101 -wd4505
-set COMMONFLAGS=-MTd -nologo -FC -W4 -WX -Oi -GR- -EHa- -D_HAS_EXCEPTIONS=0 %WARNINGFLAGS%
+set COMMONFLAGS=-MTd -nologo -FC -W4 -WX -Oi -GR- -EHa- -D_HAS_EXCEPTIONS=0 -D_CRT_SECURE_NO_WARNINGS %WARNINGFLAGS%
 set LINKERFLAGS=/opt:ref /incremental:no
 
 set CLANGFLAGS=-fdiagnostics-absolute-paths -Wno-missing-braces -Wno-unused-variable -Wno-unused-function -Wno-missing-field-initializers
@@ -19,7 +19,7 @@ set COMPILER=cl
 
 set DEBUGFLAGS=-DDEBUG=1 -Z7 -Od
 set RELEASEFLAGS=-O2
-set CFGFLAGS=-D_CRT_SECURE_NO_WARNINGS %DEBUGFLAGS%
+set CFGFLAGS=%DEBUGFLAGS%
 (echo ";r;rel;release;" | findstr /i ";%1;" 1>nul 2>nul) && (
     set CFGFLAGS=%RELEASEFLAGS%
 )
