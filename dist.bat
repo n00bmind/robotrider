03ed86b9e868fb088680b2904d3d8d379cfa22a9
n00bmind <n00bmindr0b0t@gmail.com>
2021-02-22 08:55:35 +0000

    Update dist files

--- dist.bat
+++ dist.bat
@@ -1,8 +1,6 @@
 @echo off
-build.py --debug -v
-cp bin\launcher.exe dist\launcher.debug.exe
-cp bin\robotrider.dll dist\robotrider.debug.dll
-build.py --release -v
+build.py --dev
+mkdir dist
 cp bin\launcher.exe dist\launcher.exe
 cp bin\robotrider.dll dist\robotrider.dll
 
